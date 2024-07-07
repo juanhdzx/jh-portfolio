8703112121786c8e6c71a18c891065abfae54fa1
juanhdzx <jhernandez12902@gmail.com>
2024-07-07 19:36:40 -0400

    about me and css updates

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+// import selfPortrait from '../img/about/woman.png';
+
+import { Link } from 'react-router-dom';
+
+
 const About = () => {
-    return <section className='section bg-yellow-200'>About</section>
+    return  <section className='section'>
+        <div className='container mx-auto h-full relative'>
+            {/* text and pic wrapper */}
+            <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+                {/* image */}
+                <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
+                    {/* <img  src={selfPortrait} alt='' /> */}
+                    <img  src="https://qph.cf2.quoracdn.net/main-qimg-71945a3866d5d728a0b4d892a5f1f7d1-pjlq"/>
+                </div>
+                {/* text */}
+                <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
+                    <h1 className='h1' > About me </h1>
+                    <p> some bulls hit about me em dhbsjdbcbhjbvjvberjhbivdfbv bdhbvb bci chbbchsc cbubbc </p>
+                    <br />
+                    <br />
+                    <p> more bs here sndb vustdv beduy</p>
+                    < Link to={'/portfolio'}> View my work </Link>
+                </div>
+            </div>
+        </div>
+        </section>
 };
 
 export default About;
+
+
+// july 7, i left off at 36:17
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
+// importing images from img file, i need pics from jocey
+// import selfPortrait from '../img/home/portrait.png';
+
+//importing link
+import { Link } from 'react-router-dom';
+
+
 const Home = () => {
-return <section className='section bg-blue-200'>Home</section>;
+return <section className='section'>
+            <div className='container mx-auto'>
+                {/* text & img wrapper */}
+                <div className='flex flex-col justify-center'>
+                    {/* portrait text */}
+                    <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+                        <h1 className='h1'> photographer <br /> & filmaker / whatevsss </h1>
+                        <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'> Charlotte, NC </p>
+                        <Link to={'/contact'} className='btn mb-[30px]'> hire me.. </Link>
+                    </div>
+                    {/* portrait image */}
+                    <div className='flex justify-end max-h-96 lg:max-h-max'>
+                        <div className='relative lg:-right-40 overflow-hidden'>
+                            {/* <img  src={selfPortrait} alt="portrait img"/> */} 
+                            <img  src="https://qph.cf2.quoracdn.net/main-qimg-71945a3866d5d728a0b4d892a5f1f7d1-pjlq"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>;
 
 };
 
